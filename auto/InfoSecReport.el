379c72190bdc94ecccda4d76a3c7d8d3ff5bca12
Abraham Xiao <q289196573@Gmail.com>
2013-12-06 22:35:03 +0400

    Work hard. But no need to play hard. Since you are working at UAE, you are working for much more than Dirhams. You are and you should work for Knowledge. You bring knowledge to Emirati, thus you are bringing happiness to them.
    Reviewed-by: Abraham Xiao <q289196573@Gmail.com>

--- auto/InfoSecReport.el
+++ auto/InfoSecReport.el
@@ -3,17 +3,24 @@
     (LaTeX-add-bibliographies
      "IEEEabrv"
      "Reference")
+    (LaTeX-add-environments
+     "definition")
     (LaTeX-add-labels
      "sec:introduction"
      "sec:publ-key-crypt"
      "sec:rsa-cryptosystem"
      "sec:el-gamal-crypt"
      "sec:diff-hellm-probl"
+     "sec:diff-hellm-probl-1"
+     "sec:diff-hellm-probl-2"
      "eq:2"
      "eq:3"
      "fig:diffie-key-agreement"
      "sec:disctr-logar-probl"
+     "def:Generalized-DLP"
      "eq:4"
+     "sec:exhaustive-search"
+     "sec:baby-step-giant"
      "sec:implementation")
     (TeX-run-style-hooks
      "hyperref"
