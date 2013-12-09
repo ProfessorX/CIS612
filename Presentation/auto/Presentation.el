8b2e050806b974e00fefc148f0219d9143c91104
Abraham Xiao <q289196573@Gmail.com>
2013-12-10 01:28:30 +0400

    Sleeping time. Well, I have to write something to justify and clarify a lot of things. But for now I do not have the time (and energy if I have to mention......) to do these stuff. How about writing a FAQ on your personal website or use the god damn Github as a blogging system?
    Google it!
    Search The Fucking Web!
    Read The Fucking Manual!

--- Presentation/auto/Presentation.el
+++ Presentation/auto/Presentation.el
@@ -2,6 +2,12 @@
  (lambda ()
     (LaTeX-add-bibliographies
      "Ref")
+    (LaTeX-add-labels
+     "sec:discrete-logarithm"
+     "eq:diffie–hellman-1"
+     "eq:diffie–hellman-2"
+     "sec:elgamal-cryptosystem"
+     "fig:basic-elgamal-encryption")
     (TeX-run-style-hooks
      "algorithmic"
      "amssymb"
