0b5b0067ee3801b86dda54c6dde2a6eb89b0db74
Abraham Xiao <q289196573@Gmail.com>
2013-12-10 12:36:40 +0400

    Lunch time and finish the rest. Well, work hard. Since it's the age of competition, real competition.

--- Presentation/auto/Presentation.el
+++ Presentation/auto/Presentation.el
@@ -7,7 +7,15 @@
      "eq:diffie–hellman-1"
      "eq:diffie–hellman-2"
      "sec:elgamal-cryptosystem"
-     "fig:basic-elgamal-encryption")
+     "fig:basic-elgamal-encryption"
+     "fig:basic-elgamal-public-key-encry-algo"
+     "eq:elgamal-encryption-1"
+     "eq:elgamal-encryption-2"
+     "eq:elgamal-encryption-3"
+     "eq:elgamal-encryption-4"
+     "eq:elgamal-encryption-5"
+     "eq:elgamal-encryption-6"
+     "fig:attack-power-of-two")
     (TeX-run-style-hooks
      "algorithmic"
      "amssymb"
--- Presentation/auto/Ref.el
+++ Presentation/auto/Ref.el
@@ -2,5 +2,6 @@
  (lambda ()
     (LaTeX-add-bibitems
      "Menezes:1996:HAC:548089"
-     "Rosen:2012")))
+     "Rosen:2012"
+     "ref:Elgamal1985")))
 
