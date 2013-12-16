b60a280511b84b992f51734ee3c85c68a1f11a01
Abraham Xiao <q289196573@Gmail.com>
2013-12-17 01:52:15 +0400

    Sleeping time. And you have homework. Do not forget that you have promised Dr. Zeyar to finish the "remaining" homework. Besides, you have some fucking research to do. Fucking mad, mad!

--- Final-Exam/auto/Review-Notes.el
+++ Final-Exam/auto/Review-Notes.el
@@ -3,7 +3,11 @@
     (LaTeX-add-labels
      "algo:rc4-keystream-byte"
      "algo:tea-encryption"
-     "algo:keygen-for-rsa-encrypt")
+     "algo:keygen-for-rsa-encrypt"
+     "fig:mutual-authentication-session-key"
+     "fig:simplified-ssh"
+     "fig:simplified-ssl"
+     "sec:simple-ssl-in-nutshell")
     (TeX-run-style-hooks
      "algorithmic"
      "algorithm"
