e57e14a54323c3fb7620ae72093a80c0b8ab2155
Abraham Xiao <q289196573@Gmail.com>
2013-12-17 12:20:53 +0400

    Yet another makeup you have to do. Org-mode in Emacs is wonderful. And in this manner it is "of your best interest" to learn it, use it and master it.
    Always be self-motivating requires us never give up. Chinese should not
    give up, right?

--- Homework/auto/InfoSec-HW3.el
+++ Homework/auto/InfoSec-HW3.el
@@ -1,8 +1,13 @@
 (TeX-add-style-hook "InfoSec-HW3"
  (lambda ()
+    (LaTeX-add-labels
+     "sec:authentication"
+     "sec:que-7.9.5"
+     "sec:que-7.9.5-2:algo:crack-passwords-1")
     (TeX-run-style-hooks
      "hyperref"
-     "algorithmicx"
+     "algorithmic"
+     "algorithm"
      "amssymb"
      "amsmath"
      "latex2e"
